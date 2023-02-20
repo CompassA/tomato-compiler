df12a2b3aad27dd44f458a15665eb3ca099ff7e9
Tomato <gyx645378029@163.com>
2023-02-20 22:48:42 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/*
+ * @Author: Tomato
+ * @Date: 2023-02-20 22:15:18
+ * @LastEditTime: 2023-02-20 22:15:19
+ */
+#include <iostream>
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+}
